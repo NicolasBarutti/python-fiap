c1bd15b44664f717008a7b2ad34a7c9aa7ccd858
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-08 10:19:20 -0300

    exercicio 3

--- busca/lista9/exercicio3.py
+++ busca/lista9/exercicio3.py
@@ -2,5 +2,16 @@
 # lista A contendo ou não valores repetidos e retorna uma outra lista contendo todos os
 # elementos de A sem repetição.
 
+def eliminaNumero(vet):
+    novoVetor = []
+    for i in vet:
+        if i not in novoVetor:
+            novoVetor.append(i)
+    return novoVetor
 
-vet = [1, 2, 3, 4, 5, 6, 6, 7, 7,8]
+
+vet = [1, 2, 3, 4, 5, 6, 6, 7, 7, 8]
+
+
+resultado = eliminaNumero(vet)
+print(resultado)
